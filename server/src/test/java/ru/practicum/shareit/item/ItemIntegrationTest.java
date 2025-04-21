55c20768f607d9558914c79d5cfdd5f9f91d4f73
Valentine B <vanta11a@yandex.ru>
2025-04-21 22:16:47 +0500

    Доработки по ТЗ 16 спринта: кодстайл

--- server/src/test/java/ru/practicum/shareit/item/ItemIntegrationTest.java
+++ server/src/test/java/ru/practicum/shareit/item/ItemIntegrationTest.java
@@ -1,20 +1,12 @@
 package ru.practicum.shareit.item;
 
 import jakarta.persistence.EntityManager;
-import jakarta.persistence.TypedQuery;
 import lombok.RequiredArgsConstructor;
-import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.context.ActiveProfiles;
 import org.springframework.transaction.annotation.Transactional;
 import ru.practicum.shareit.item.dto.ItemDto;
-import ru.practicum.shareit.item.model.Item;
-import ru.practicum.shareit.item.service.ItemServiceImpl;
-import ru.practicum.shareit.user.model.User;
-
-import static org.junit.jupiter.api.Assertions.assertEquals;
-import static org.junit.jupiter.api.Assertions.assertNotNull;
 
 @Transactional
 @ActiveProfiles("test")
